c9325ffeb64dcb3685e221904ce52fdc4ea0c9a7
hnp <hobnobpirate@gmail.com>
2019-07-17 23:46:05 -0400

    Better documentation of venom class.

--- venominator/venom.py
+++ venominator/venom.py
@@ -7,20 +7,46 @@ from venominator import attributes
 
 
 class Venom(object):
-    """Venom object stores attributes to be used in creating msfvenom payload"""
+    """Venom object stores attributes to be used in creating msfvenom payload
+    
+    :param arch<str>: architecture
+    :param badchars<str>: list of bad characters for transform formats
+    :param encoder<str>: encoder to use
+    :param format<str>: output format
+    :param iterations<int>: number of times to run encoder
+    :param address<str>: address to call back to
+    :param payload<str>: payload to build
+    :param port<int>: port to listen on or call back to
+    :param platform<str>: platform to build for
+    :param outfile<str>: filename to output to
+    :param additional<str>: additional options to add
+    """
 
-    def __init__(self):
-        self.arch = None
-        self.badchars = None
-        self.encoder = None
-        self.format = None
-        self.iterations = None
-        self.address = None
-        self.payload = None
-        self.port = None
-        self.platform = None
-        self.outfile = None
-        self.additional = None
+    def __init__(
+        self,
+        arch: str = None,
+        badchars: str = None,
+        encoder: str = None,
+        format: str = None,
+        iterations: int = None,
+        address: str = None,
+        payload: str = None,
+        port: int = None,
+        platform: str = None,
+        outfile: str = None,
+        additional: str = None,
+    ):
+        self.arch = arch
+        self.badchars = badchars
+        self.encoder = encoder
+        self.format = format
+        self.iterations = iterations
+        self.address = address
+        self.payload = payload
+        self.port = port
+        self.platform = platform
+        self.outfile = outfile
+        self.additional = additional
 
     def build_cmd(self) -> None:
         """Builds the CLI command to generate the payload"""
